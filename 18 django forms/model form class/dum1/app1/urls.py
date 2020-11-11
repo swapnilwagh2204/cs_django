663e540b2834930c4030063d4721eeb49c025067
swap <swapnilswagh1234@gmail.com>
2020-11-11 15:02:00 +0530

    django model forms updated

--- 18 django forms/model form class/dum1/app1/urls.py
+++ 18 django forms/model form class/dum1/app1/urls.py
@@ -2,7 +2,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
-    path("form/",views.emp),
-    path("formpost/",views.pstusr),
+urlpatterns = [
+    path("form/", views.emp),
+    path("formpost/", views.pstusr),
+    path("dispstu/", views.disstu),
+    path("disppost/", views.dispost),
 ]
